22edce41d6b35e804dbe64f1fbb72ce8f22a8e2c
Anton Eremenko <eremenkoaa@tmn.lukoil.com>
2015-02-19 14:09:03 +0500

    init

---
+++ src/main/java/com/eldest/App.java
@@ -0,0 +1,13 @@
+package com.eldest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
